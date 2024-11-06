36c332ecbe8c8211bfef7a41969d73b65f3fca7c
Vukovic Nikola <admin@nikolav.rs>
2024-11-06 20:36:41 +0100

    Wed, Nov  6, 2024  7:36:40 PM

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
-FROM node:alpine
+FROM node:16-alpine
 
+RUN mkdir -p /home/app/public
 WORKDIR /home/app
 
 # install
--- push.sh
+++ push.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 APP_HOST=<ADDRESS:IP>
-APP_PATH=/root/app/<DIR>/public
-scp -r ./dist/* root@$APP_HOST:$APP_PATH
+APP_PATH=/root/app/<DIR>
+scp -r ./public/* root@$APP_HOST:$APP_PATH/public
