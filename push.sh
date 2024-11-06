767aacb69f54a5adecae2b6004ea542339662b11
Vukovic Nikola <admin@nikolav.rs>
2024-11-06 20:54:51 +0100

    Wed, Nov  6, 2024  7:54:51 PM

--- push.sh
+++ push.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
-APP_HOST=<ADDRESS:IP>
-APP_PATH=/root/app/<DIR>
+APP_HOST=78.141.241.165
+APP_PATH=/root/app/docker-express-static
 scp -r ./public/* root@$APP_HOST:$APP_PATH/public
