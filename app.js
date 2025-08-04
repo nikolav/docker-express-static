abd50f2af8295fede1d03962988d776503539878
Vukovic Nikola <admin@nikolav.rs>
2025-08-04 16:47:08 +0200

    add compression

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
+// #!/usr/bin/env node
+
 var express = require("express");
 var path = require("path");
 var logger = require("morgan");
+var compression = require("compression");
 
 var app = express();
 
+// enable gzip compression
+app.use(compression());
 app.use(logger("dev"));
 app.use(express.static(path.join(__dirname, "public")));
+
+// @404
+app.use(function (req, res, next) {
+  res.status(404).send("Not Found");
+});
+
+// @error
+app.use(function (err, req, res, next) {
+  console.error(err);
+  res.status(500).send("Internal Server Error");
+});
 
 module.exports = app;
